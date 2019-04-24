476d64c7ab1764eb20dcc702ab6e1ac46de64a50
Harry Upton <harryupton100@gmail.com>
2019-04-24 18:21:00 +0100

    ScreenPosGetter.py
    Added comment to explain what it does.

--- ScreenPosGetter.py
+++ ScreenPosGetter.py
@@ -1,3 +1,6 @@
+# This script repeatedly prints the pixel coordinates of the mouse on the screen,
+# to make it easier to specify the region the mining script should dig within.
+
 from pynput.keyboard import Key, Controller as kc
 from pynput.mouse import Button, Controller as mc
 import time
